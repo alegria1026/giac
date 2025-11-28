9c46421b9c37ec3fc8dcc0f8a3fa7a553a11c688
alegria1026 <yahirhumberto04@gmail.com>
2025-11-28 00:43:31 -0600

    Subiendo archivos del proyecto

---
+++ database/migrations/2025_11_28_044248_create_projects_table.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('projects', function (Blueprint $table) {
+            $table->id();
+            $table->foreignId('user_id')->constrained('users');
+            $table->string('name')->nullable(false);
+            $table->string('description')->nullable(false);
+            $table->enum('status', ['Ingeniería', 'Construcción']);
+            $table->string('attached_file_1', 255)->nullable(false);
+            $table->string('attached_file_2', 255)->nullable();
+            $table->string('attached_file_3', 255)->nullable();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('projects');
+    }
+};
