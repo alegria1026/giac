255644ef7f25539526b460b62a086f86698f8a5e
alegria1026 <yahirhumberto04@gmail.com>
2025-11-28 22:23:03 -0600

    create model project and certification

---
+++ app/Models/Project.php
@@ -0,0 +1,16 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Model;
+
+class Project extends Model
+{
+    protected $fillable = [
+        'user_id',
+        'name',
+        'description',
+        'category',
+        'attached_file',
+    ];
+}
