43756e793b54a44a970dd23b83382dca94913b93
alegria1026 <yahirhumberto04@gmail.com>
2025-12-02 14:22:25 -0600

    update servicecontroller

--- app/Http/Controllers/ServiceController.php
+++ app/Http/Controllers/ServiceController.php
@@ -102,7 +102,7 @@ class ServiceController extends Controller
     /**
      * Display the specified resource.
      */
-    public function show(string $id)
+    public function show($id)
     {
         $service = Service::findOrFail($id);
 
