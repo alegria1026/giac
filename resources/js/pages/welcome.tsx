3aa93bfca06ea93b44cd3634af6bd87a1032c45e
diana <zS22017039@estudiantes.uv.mx>
2025-12-07 14:39:04 -0600

    Sección de certificaciones

--- resources/js/pages/welcome.tsx
+++ resources/js/pages/welcome.tsx
@@ -35,10 +35,6 @@ export default function Welcome({
 
             <div className="flex min-h-screen flex-col items-center bg-[#FDFDFC] p-6 text-[#1b1b18] lg:justify-center lg:p-8 dark:bg-[#0a0a0a]">
 
-                {/* ❌ BORRADO: El header viejo del template
-                    (porque ya tienes tu propio Header.tsx nuevo)
-                */}
-
                 <div className="flex w-full items-center justify-center opacity-100 transition-opacity duration-750 lg:grow starting:opacity-0">
                     <main className="flex w-full max-w-[335px] flex-col lg:max-w-4xl">
 
