0ca722d044163cb902660b8c66dc161f72b2e574
cabrademontes <cabrademontes@gmail.com>
2025-12-08 15:45:01 -0600

    viteReactRefresh, added this again, fix some issues in the view

--- resources/views/app.blade.php
+++ resources/views/app.blade.php
@@ -39,10 +39,11 @@
         <link rel="preconnect" href="https://fonts.bunny.net">
         <link href="https://fonts.bunny.net/css?family=instrument-sans:400,500,600" rel="stylesheet" />
 
+        @viteReactRefresh <!-- esta linea ocasionaba que fallara algo de la apariencia, mostraba la pantalla blanca -->
         @vite(['resources/js/app.tsx', "resources/js/pages/{$page['component']}.tsx"])
         @inertiaHead
     </head>
     <body class="font-sans antialiased">
-        @inertia
+    @inertia
     </body>
 </html>
