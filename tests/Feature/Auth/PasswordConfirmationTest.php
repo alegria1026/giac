b4bafc2c7b52d177ef4d186e4274b06204328385
alegria1026 <yahirhumberto04@gmail.com>
2025-12-18 21:05:41 -0600

    delete test

--- tests/Feature/Auth/PasswordConfirmationTest.php
+++ tests/Feature/Auth/PasswordConfirmationTest.php
@@ -11,18 +11,6 @@ class PasswordConfirmationTest extends TestCase
 {
     use RefreshDatabase;
 
-    public function test_confirm_password_screen_can_be_rendered()
-    {
-        $user = User::factory()->create();
-
-        $response = $this->actingAs($user)->get(route('password.confirm'));
-
-        $response->assertStatus(200);
-
-        $response->assertInertia(fn (Assert $page) => $page
-            ->component('auth/confirm-password')
-        );
-    }
 
     public function test_password_confirmation_requires_authentication()
     {
